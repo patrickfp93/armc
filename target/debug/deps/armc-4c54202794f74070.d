4750d268d99d9feeb882c737feb4a61dd3796539
Patrick Fernandes Peçanha <patrickfp93@gmail.com>
2022-11-24 15:53:15 -0300

    Segundo debug.

--- target/debug/deps/armc-4c54202794f74070.d
+++ target/debug/deps/armc-4c54202794f74070.d
@@ -1,6 +1,6 @@
-c:\Rust_Projects\Projects\armc\target\debug\deps\armc-4c54202794f74070.exe: src\lib.rs src\tests.rs src\core.rs src\option_cell.rs
+c:\Rust_Projects\GitProjects\armc\target\debug\deps\armc-4c54202794f74070.exe: src\lib.rs src\tests.rs src\core.rs src\option_cell.rs
 
-c:\Rust_Projects\Projects\armc\target\debug\deps\armc-4c54202794f74070.d: src\lib.rs src\tests.rs src\core.rs src\option_cell.rs
+c:\Rust_Projects\GitProjects\armc\target\debug\deps\armc-4c54202794f74070.d: src\lib.rs src\tests.rs src\core.rs src\option_cell.rs
 
 src\lib.rs:
 src\tests.rs:
